2314d3d2a25d4774f6ef16437f1306f93a4b1dcf
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-27 21:22:45 +0300

    add: banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        titillilum: ['Titillium Web', 'sans-serif'],
+        titillilum: ["Titillium Web", "sans-serif"],
       },
       colors: {
         conduit: {
-          green: '#5CB85C',
+          green: "#5CB85C",
         },
       },
       spacing: {
-        navItem: '0.425rem',
+        navItem: "0.425rem",
+      },
+      boxShadow: {
+        banner:
+          "inset 0 8px 8px -8px rgb(0 0 0 / 30%), inset 0 -8px 8px -8px rgb(0 0 0 / 30%)",
+      },
+      dropShadow: {
+        logo: "0px 1px 3px rgb(0 0 0 / 30%)",
+      },
+      fontSize: {
+        logo: "3.5rem",
       },
     },
   },
