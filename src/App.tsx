858268c02f5f4742a001e6f53903c7c22198f7fd
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-28 02:17:43 +0300

    change: 'get' method by defaul

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 import { Header } from "./common/components/header/header.component";
 import { Banner } from "./common/components/banner/banner.component";
-import { Article } from "./modules/feed/components/article/article.component";
 import { Feed } from "./modules/feed/components/feed/feed.component";
 
 interface AppProps {}
 
 export const App: FC<AppProps> = () => {
   return (
-    <div>
+    <div className="pb-16">
       <Header/>
       <Banner/>
       <Feed/>
