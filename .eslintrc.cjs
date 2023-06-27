196e95bcbe047931880fa89a3ca5300418ad09c7
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-27 19:27:39 +0300

    add: header

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+export default {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'no-empty-pattern': 'warn',
+    "@typescript-eslint/no-empty-interface": "off"
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Header } from "./common/components/header/header.component";
+
+interface AppProps {}
+
+export const App: FC<AppProps> = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
