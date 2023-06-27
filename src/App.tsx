75047896de8d1032763ce902905071559e9953d5
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-27 21:47:16 +0300

    add: feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { Header } from "./common/components/header/header.component";
 import { Banner } from "./common/components/banner/banner.component";
+import { Article } from "./modules/feed/components/article/article.component";
+import { Feed } from "./modules/feed/components/feed/feed.component";
 
 interface AppProps {}
 
@@ -9,6 +11,7 @@ export const App: FC<AppProps> = () => {
     <div>
       <Header/>
       <Banner/>
+      <Feed/>
     </div>
   );
 };
