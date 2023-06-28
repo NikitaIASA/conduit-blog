8bf956bb1a05b705fe70404b1a576980370531ca
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-28 17:42:32 +0300

    add: profile page v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./common/components/header/header.component";
-import { Banner } from "./common/components/banner/banner.component";
-import { Feed } from "./modules/feed/components/feed/feed.component";
+import { GlobalFeedPage } from "./modules/feed/pages/global-feed.page";
+import { ProfilePage } from "./modules/profile/pages/profile.pages";
 
 interface AppProps {}
 
 export const App: FC<AppProps> = () => {
   return (
     <div className="pb-16">
-      <Header/>
-      <Banner/>
-      <Feed/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<GlobalFeedPage />} />
+        <Route path="/:profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
