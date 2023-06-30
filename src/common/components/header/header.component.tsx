68a026f8f5f0b139c414cd9b40e3eb3bdf77b7d0
Nikita Chaika <iasastudent2021@gmail.com>
2023-06-30 23:05:39 +0300

    use-auth hook

--- src/common/components/header/header.component.tsx
+++ src/common/components/header/header.component.tsx
@@ -1,11 +1,14 @@
 import clsx from "clsx";
 import { FC } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../../../modules/auth/hooks/use-auth";
 import { Container } from "../container/container.component";
 
 interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = () => {
+  const { isLoggedIn, logOut, user } = useAuth();
+
   const navLinkClasses = ({ isActive }: { isActive: boolean }) =>
     clsx("py-navItem hover:text-black/60 hover:no-underline", {
       "text-black/30": !isActive,
@@ -29,16 +32,57 @@ export const Header: FC<HeaderProps> = () => {
                   Home
                 </NavLink>
               </li>
-              <li className="ml-4">
-                <NavLink to="/sign-in" className={navLinkClasses}>
-                  Sign in
-                </NavLink>
-              </li>
-              <li className="ml-4">
-                <NavLink to="/sign-up" className={navLinkClasses}>
-                  Sign up
-                </NavLink>
-              </li>
+              {isLoggedIn ? (
+                <>
+                  <li className="ml-4">
+                    <NavLink to="/editor" className={navLinkClasses}>
+                      <i className="mr-1 ion-compose" />
+                      New article
+                    </NavLink>
+                  </li>
+                  <li className="ml-4">
+                    <NavLink to="/settings" className={navLinkClasses}>
+                      <i className="mr-1 ion-gear-a" />
+                      Settings
+                    </NavLink>
+                  </li>
+                  <li className="ml-4">
+                    <NavLink
+                      to={`/@${user?.username}`}
+                      className={navLinkClasses}
+                    >
+                      <img
+                        src={user?.image}
+                        alt={`${user?.username} avatar`}
+                        className="w-6 h-6 rounded-full inline mr-2"
+                      />
+                      {user?.username}
+                    </NavLink>
+                  </li>
+                  <li className="ml-4">
+                    <NavLink
+                      to="/"
+                      className="py-navItem text-black/30 hover:text-black/60 hover:no-underline"
+                      onClick={logOut}
+                    >
+                      Log out
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="ml-4">
+                    <NavLink to="/sign-in" className={navLinkClasses}>
+                      Sign in
+                    </NavLink>
+                  </li>
+                  <li className="ml-4">
+                    <NavLink to="/sign-up" className={navLinkClasses}>
+                      Sign up
+                    </NavLink>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </Container>
