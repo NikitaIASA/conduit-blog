215996e0e09635188d384a850388aadfac45c71e
Nikita Chaika <iasastudent2021@gmail.com>
2023-07-06 22:47:50 +0300

    add: like-button implementation

--- src/modules/feed/api/utils.ts
+++ src/modules/feed/api/utils.ts
@@ -1,8 +1,70 @@
-import { GlobalFeedInDTO } from './dto/global-feed.in';
+import { Draft } from 'immer';
+import { RootState } from '../../../store/store';
+import { FeedArticle, GlobalFeedInDTO } from './dto/global-feed.in';
+import { SingleArticleInDTO } from './dto/single-article.in';
+import { FeedData } from './repository';
 
 export const transformResponse = (response: GlobalFeedInDTO) => {
     return {
         articles: response.articles || [],
         articlesCount: response.articlesCount || 0,
     };
+};
+
+const updateFeed = <Q>(
+    feedKey: string,
+    data: { article: FeedArticle },
+    feedKeys: string[],
+    state: RootState,
+    dispatch: any,
+    feedApi: any
+) => {
+    for (
+        let i = 0, key = feedKeys[i], queryItem = state.feedApi.queries[key];
+        i < feedKeys.length;
+        i++, key = feedKeys[i], queryItem = state.feedApi.queries[key]
+    ) {
+        if (!key.startsWith(feedKey)) {
+            continue;
+        }
+
+        dispatch(
+            feedApi.util.updateQueryData(
+                feedKey,
+                queryItem!.originalArgs as Q,
+                (draft: Draft<FeedData> | Draft<SingleArticleInDTO>) => {
+                    if ('articles' in draft) {
+                        const updateId = draft.articles.findIndex(
+                            (article) => article.slug === data.article.slug
+                        );
+
+                        if (updateId >= 0) {
+                            draft.articles[updateId] = data.article;
+                        }
+                    } else {
+                        draft.article.favorited = data.article.favorited;
+                        draft.article.favoritesCount = data.article.favoritesCount;
+                    }
+                }
+            )
+        );
+    }
+};
+
+export const replaceCachedArticle = async (
+    getState: any,
+    queryFulfilled: any,
+    dispatch: any,
+    feedApi: any
+) => {
+    const state = getState() as RootState;
+
+    try {
+        const { data } = await queryFulfilled;
+        const feedKeys = Object.keys(state.feedApi.queries);
+
+        updateFeed('getGlobalFeed', data, feedKeys, state, dispatch, feedApi);
+        updateFeed('getProfileFeed', data, feedKeys, state, dispatch, feedApi);
+        updateFeed('getSingleArticle', data, feedKeys, state, dispatch, feedApi);
+    } catch (e) { }
 };
